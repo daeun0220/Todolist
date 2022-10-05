1e8077386e921d4b5c4e7a340d5220fed9ba82f3
daeun0220 <daeunkim1284@gmail.com>
2022-10-05 16:31:10 +0900

    authcontroller

--- src/main/java/com/example/todolist/dto/RegisterResponse.java
+++ src/main/java/com/example/todolist/dto/RegisterResponse.java
@@ -1,20 +1,17 @@
 package com.example.todolist.dto;
 
 import com.example.todolist.domain.Member;
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 
 @Data
-
 public class RegisterResponse {
 
     private Long memberId;
     private String email;
     private String nickname;
 
-    //TODO: 생성자 만들어보기
-    public RegisterResponse(Member member){
-
+    public RegisterResponse(Member member) { // 생성자 AuthService 에서 Member 담아야해서 만들어줘야함
         this.memberId = member.getId();
         this.email = member.getEmail();
         this.nickname = member.getNickname();
--- src/main/java/com/example/todolist/dto/TodoCreateRequestDto.java
+++ src/main/java/com/example/todolist/dto/TodoCreateRequestDto.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class TodoCreateRequestDto { //계층간의 데이터를 옮겨줄 때
     private Long memberId;
-    private String contents;
+    private String contents;   // flag 있어야하지않나..?
 
 }
